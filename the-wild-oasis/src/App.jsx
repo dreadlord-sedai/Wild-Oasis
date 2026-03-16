c52eef21a7a2c663f07b27dc133571a41742dc3a
dreadlord-sedai <dahamifabbio@gmail.com>
2026-03-16 09:13:14 +0530

    Setup Styled Components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -17,15 +17,16 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<AppLayout />}>
-            <Route index element={<Dashboard />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
           </Route>
+
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
